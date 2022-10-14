0f897826dadc451990b241dc732e7de5ff1e1a13
Aliyah <Aliyahjohnx@gmail.com>
2022-10-14 01:30:22 +0000

    new python v

--- App/models/user.py
+++ App/models/user.py
@@ -5,7 +5,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username =  db.Column(db.String, nullable=False)
     email =  db.Column(db.String, nullable=False)
-    password = db.Column(db.String(120), nullable=False)
+    password = db.Column(db.String, nullable=False)
 
 
     def __init__(self, username, email, password):
