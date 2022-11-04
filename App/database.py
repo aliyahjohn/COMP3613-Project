edc5bd24476b7329cb702113891dc8e176510a07
CAJames1995 <illred@hotmail.com>
2022-11-04 01:57:41 +0000

    db.create_all

--- App/database.py
+++ App/database.py
@@ -9,7 +9,7 @@ def get_migrate(app):
 def create_db(app):
     db.init_app(app)
     
-    # db.create_all(app=app)
+    db.create_all(app=app)
     # CAUSING ERROR: 
     # AttributeError: 'NoneType' object has no attribute 'drivername'
     
